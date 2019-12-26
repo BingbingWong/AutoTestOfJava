050ec2387f0f5645e04537f4ad40c3a7694b3ace
BingbingWong <540144921@qq.com>
2019-12-26 09:41:04 +0800

    HttpClient部署

---
+++ Chapter9/src/main/java/com/course/httpclient/demo/MyHttpClient.java
@@ -0,0 +1,24 @@
+package com.course.httpclient.demo;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class MyHttpClient {
+    @Test
+    public void test() throws IOException {
+        //用来存放我们的结果
+        String result;
+        HttpGet get=new HttpGet("https://www.baidu.com");
+        //用来执行get方法
+        HttpClient client=new DefaultHttpClient();
+        HttpResponse response=client.execute(get);
+        //获取响应内容,用实体转换字符串输出
+        result=EntityUtils.toString(response.getEntity(),"utf-8");
+        System.out.println(result);
+    }
+}
